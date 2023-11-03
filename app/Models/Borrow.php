aea156b7edc56ce5b0f1cb03eea7c4600adcabab
shota <shota19830706@gmail.com>
2023-11-04 06:06:50 +0900

    Define fillable property in borrow model

--- app/Models/Borrow.php
+++ app/Models/Borrow.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Borrow extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'item_name',
+        'borrowed_at',
+        'trust_score',
+    ];
 }
