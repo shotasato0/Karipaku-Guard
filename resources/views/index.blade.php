0c3d62cb3e27b8873c3f3e7bb43564833fd496aa
shota <shota19830706@gmail.com>
2023-11-05 14:07:12 +0900

    Add main content

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -17,6 +17,7 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="antialiased">
+        {{-- header --}}
         <header class="text-gray-600 body-font">
             <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
               <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
@@ -49,7 +50,60 @@
                 </div>
             @endif
 
-            
+            {{-- main --}}
+            <div class="container mx-auto p-8">
+                <div class="flex flex-col">
+                  <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+                      <div class="overflow-hidden">
+                        <table class="min-w-full">
+                          <thead class="bg-gray-100">
+                            <tr>
+                              <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                借りた人の名前
+                              </th>
+                              <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                借りた物の名前
+                              </th>
+                              <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                借りた日
+                              </th>
+                              <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                経過日数
+                              </th>
+                              <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                信頼度スコア
+                              </th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            <!-- 繰り返しのアイテムデータがここに入ります -->
+                            <tr class="bg-white border-b">
+                              <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                                山田 太郎
+                              </td>
+                              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                ファイナルファンタジーXV
+                              </td>
+                              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                2023-01-15
+                              </td>
+                              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                30日
+                              </td>
+                              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                80
+                              </td>
+                            </tr>
+                            <!-- 他のアイテム行も同様に -->
+                          </tbody>
+                        </table>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              
         </div>
     </body>
 </html>
