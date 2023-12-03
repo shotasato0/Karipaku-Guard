9811ffe1bbf2f5ae8c95ea91d7cad59a32618b6e
shota <shota19830706@gmail.com>
2023-12-04 06:28:14 +0900

    Implementation of search function

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -3,21 +3,61 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Borrow;
 use App\Models\Friend;
+use App\Models\Borrow;
 
 class SearchController extends Controller
 {
     public function index(Request $request)
-{
-    $keyword = $request->input('keyword');
+    {
+        // $keyword = $request->input('keyword');
+        // $friends = collect();
+        // $borrows = collect();
 
-    $friends = Friend::where('name', 'like', '%'.$keyword.'%')->get();
-    $borrow = Borrow::where('name', 'like', '%'.$keyword.'%')->get();
-    
+        // if (!empty($keyword)) {
+        //     $friends = Friend::where('name', 'like', '%'.$keyword.'%')->paginate(5);
+        //     $borrows = Borrow::where('item_name', 'like', '%'.$keyword.'%')->paginate(5);
+        // }
 
-    // 検索結果をビューに渡す
-    return view('search.results', compact('friends'));
-}
+        // // 検索結果をビューに渡す
+        // return view('search.results', compact('friends', 'borrows'));
 
+        //テーブルからすべてのレコードを取得
+        $borrows = Borrow::query();
+
+        $keyword = $request->input('keyword'); //nameキーワードを取得
+        $upper = $request->input('upper');
+        $upper = $request->input('lower');
+
+        //キーワードから検索処理
+        if(!empty($keyword)) {
+
+            $borrows->where('item_name', 'LIKE', "%{$keyword}%")
+            ->orwhereHas('friend', function ($query) use ($keyword) {
+                $query->where('name', 'LIKE', "%{$keyword}%");
+            })->get();
+            }
+
+            //最大値から検索処理
+            if(!empty($upper)) {
+
+                $borrows->whereHas('friend', function ($q)use($upper) {
+                    $q->where('id', '<=', $upper);
+                })->get();
+            }
+
+            //最小値から検索
+            if(!empty($lower)) {
+
+                $borrows->whereHas('friend', function ($q)use($lower) {
+                    $q->where('id', '<=', $lower);
+                })->get();
+            }
+
+            //ページネーション
+            //5レコードずつ表示する
+            $posts = $borrows->paginate(5);
+            // dd($posts);
+            return view('search.results', ['posts' => $posts]);
+    }
 }
---
+++ resources/views/search/results.blade.php
@@ -0,0 +1,107 @@
+<x-header>
+    <x-slot name="title" class="text-2xl font-bold my-4">Karipaku Guard</x-slot>
+    <div class="container mx-auto p-12">
+        <div class="flex flex-col">
+            <div class="flex justify-end mb-4">
+                <div class="flex items-center mx-2">
+                    <form action="{{ route('search.index') }}" method="GET">
+
+                        @csrf
+
+                        <div class="relative mr-4">
+                            <input type="text" placeholder="検索" name="keyword"
+                                class="px-4 py-2 mr-48 border border-gray-300 rounded-md focus:outline-none focus:ring-2
+                                focus:ring-blue-500 focus:border-transparent w-full">
+                            <input type="submit" value="検索"
+                                class="absolute right-0 top-0 h-full bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold px-4 py-2 rounded-r-md cursor-pointer">
+                        </div>
+                    </form>
+                    <a href="{{ route('borrows.create') }}"
+                        class="bg-green-500 hover:bg-blue-700 text-white font-bold py-2 px-3 rounded-md">
+                        新規作成
+                    </a>
+                </div>
+            </div>
+            <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+                <div class="py-4 inline-block min-w-full sm:px-6 lg:px-10">
+                    <div class="overflow-hidden shadow-md rounded-lg">
+                        <table class="min-w-full">
+                            <thead class="bg-gray-200">
+                                <tr>
+                                    <th scope="col" class="text-lg  font-semibold text-gray-900 px-6 py-4 text-left">
+                                        借りた人の名前
+                                    </th>
+                                    <th scope="col" class="text-lg font-semibold text-gray-900 px-6 py-4 text-left">
+                                        借りた物の名前
+                                    </th>
+                                    <th scope="col" class="text-lg font-semibold text-gray-900 px-6 py-4 text-left">
+                                        借りた日
+                                    </th>
+                                    <th scope="col" class="text-lg font-semibold text-gray-900 px-6 py-4 text-left">
+                                        経過日数
+                                    </th>
+                                    <th scope="col" class="text-lg font-semibold text-gray-900 px-6 py-4 text-left">
+                                        信頼度
+                                    </th>
+                                    <th scope="col" class="text-lg font-semibold text-gray-900 px-2 py-4 text-left">
+                                        編集
+                                    </th>
+                                    <th scope="col" class="text-lg font-semibold text-gray-900 py-4 text-left">
+                                        削除
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @forelse ($posts as $borrow)
+                                    <tr class="bg-white border-b">
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                                            <a href="{{ route('borrows.friend', $borrow->id) }}"
+                                                class="hover:text-blue-600">
+                                                {{ $borrow->friend->name }}
+                                            </a>
+                                        </td>
+                                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                            {{ $borrow->item_name }}
+                                        </td>
+                                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                            {{-- {{ $borrow->borrowed_at->format('Y-m-d') }} --}}
+                                        </td>
+                                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                            {{ $borrow->days_passed }}
+                                        </td>
+                                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                            {{ $borrow->trust_score }}
+                                        </td>
+                                        <td class="text-sm text-gray-900 font-light py-4 whitespace-wrap">
+                                            <a href="{{ route('borrows.edit', $borrow) }}"
+                                                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 rounded inline-block align-middle text-center"
+                                                style="line-height: normal; height: 38px; width: 80px;">
+                                                編集
+                                            </a>
+                                        </td>
+                                        <td class="text-sm text-gray-900 font-light py-4 whitespace-wrap">
+                                            <form action="{{ route('borrows.destroy', $borrow) }}" method="POST"
+                                                class="inline-block align-middle" id="js-borrow-delete">
+
+                                                @csrf
+                                                @method('DELETE')
+
+                                                <button type="submit"
+                                                    class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+                                                    style="line-height: normal; height: 38px; width: 80px;">
+                                                    削除
+                                                </button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @empty
+                                    <td class="text-center py-4" colspan="7">No Data</td>
+                                @endforelse
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-header>
