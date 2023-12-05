1a4b46c3dccb040bd829f06aa3dd88a822e0c217
shota <shota19830706@gmail.com>
2023-12-05 15:20:24 +0900

    Add comment to search.js

--- resources/js/search.js
+++ resources/js/search.js
@@ -1,39 +1,37 @@
+// 関数 saveKeyword を定義します。この関数はイベント e を引数として受け取ります。
 function saveKeyword(e) {
-    // console.log();
+    // デフォルトのイベント動作（ここではフォームの送信）を防止します。
     e.preventDefault();
+    // HTMLドキュメントから "js-keyword" IDを持つ要素の値（入力されたキーワード）を取得します。
     var keyword = document.getElementById("js-keyword").value;
+    // 取得したキーワードをセッションストレージに "lastKeyword" というキーで保存します。
     sessionStorage.setItem("lastKeyword", keyword);
-    // console.log("Saved keyword: " + keyword);
+
+    // オリジナルのイベントターゲット（ここではフォーム）に対して submit イベントを実行します。
     e.target.submit();
 }
 
-// グローバルスコープに関数を設定
+// グローバルスコープに saveKeyword 関数を割り当てます。
 window.saveKeyword = saveKeyword;
 
+// DOMContentLoaded イベント（HTMLドキュメントの読み込みが完了した時）に対するイベントリスナーを追加します。
 document.addEventListener("DOMContentLoaded", function () {
-    // console.log(lastKeyword);
-    // 検索結果ページに遷移した際の処理
+    // ウィンドウの現在のパスが "/search" である場合のみ以下の処理を実行します。
     if (window.location.pathname === "/search") {
+        // セッションストレージから "lastKeyword" というキーで保存されている値を取得します。
         var lastKeyword = sessionStorage.getItem("lastKeyword");
+        // lastKeyword が存在する場合（null または undefined でない場合）、
+        // HTMLドキュメントから "js-keyword" IDを持つ要素の値に lastKeyword を設定します。
         if (lastKeyword) {
             document.getElementById("js-keyword").value = lastKeyword;
-            // console.log('DOMContentLoaded event fired');
         }
-    } else {
-        sessionStorage.removeItem("lastKeyword");
     }
-
-    // ブラウザの戻るボタンの挙動を検知
-    window.addEventListener("popstate", function () {
-        console.log('popstate event fired');
-        sessionStorage.removeItem("lastKeyword");
-        document.getElementById("js-keyword").value = '';
-    });
 });
 
+// 関数 clearKeyword を定義します。この関数はセッションストレージから "lastKeyword" を削除します。
 function clearKeyword() {
     sessionStorage.removeItem("lastKeyword");
 }
 
-// グローバルスコープに関数を設定
+// グローバルスコープに clearKeyword 関数を割り当てます。
 window.clearKeyword = clearKeyword;
