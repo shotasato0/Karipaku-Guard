03506f424eee6af6ec430cd461be1fb503314ebb
shota <shota19830706@gmail.com>
2023-12-06 07:20:51 +0900

    First aid for JavaScript Null errors

--- resources/js/confirmdelete.js
+++ resources/js/confirmdelete.js
@@ -1,9 +1,15 @@
-document.getElementById("js-borrow-delete").addEventListener("submit", (e) => {
-    e.preventDefault();
+const borrowDeleteElement = document.getElementById("js-borrow-delete");
 
-    if (!confirm("削除しますか？")) {
-        return;
-    }
+if (borrowDeleteElement) {
+    borrowDeleteElement.addEventListener("submit", (e) => {
+        e.preventDefault();
 
-    e.target.submit();
-});
+        if (!confirm("削除しますか？")) {
+            return;
+        }
+
+        e.target.submit();
+    });
+} else {
+    console.log("Element with ID 'js-borrow-delete' not found");
+}
