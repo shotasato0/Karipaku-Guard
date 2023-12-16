60607484df111ece6c0e31250e80b0737c707de9
shota <shota19830706@gmail.com>
2023-12-16 09:43:23 +0900

    Adjust margins for edit and delete buttons

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -41,22 +41,22 @@
                             <tbody>
                                 @forelse ($borrows as $borrow)
                                     <tr class="bg-white border-b">
-                                        <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                                        <td data-label="借りた人の名前" class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
                                             <a href="{{ route('borrows.friend', $borrow->id) }}"
                                                 class="hover:text-blue-600">
                                                 {{ $borrow->friend->name }}
                                             </a>
                                         </td>
-                                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                        <td data-label="借りた物の名前" class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                                             {{ $borrow->item_name }}
                                         </td>
-                                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                        <td data-label="借りた日" class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                                             {{ $borrow->borrowed_at->format('Y-m-d') }}
                                         </td>
-                                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                        <td data-label="経過日数" class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                                             {{ $borrow->days_passed }}
                                         </td>
-                                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
+                                        <td data-label="信頼度" class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
                                             data-trust-score="{{ $borrow->trust_score }}">
                                             {{-- スコアの数値 --}}
                                             {{-- {{ $borrow->trust_score }}  --}}
@@ -67,13 +67,13 @@
                                             <img src="../../images/exprosion.png" alt="爆発" class="w-24 hidden"
                                                 id="js-exprosion">
                                         </td>
-                                        <td class="pl-4 py-4 text-sm text-gray-900 font-light whitespace-wrap">
+                                        <td class="edit-button pl-4 py-4 text-sm text-gray-900 font-light whitespace-wrap">
                                             <a href="{{ route('borrows.edit', $borrow) }}"
                                                 class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 rounded flex items-center justify-center h-10 w-20">
                                                 編集
                                             </a>
                                         </td>
-                                        <td class="px-4 py-4 text-sm text-gray-900 font-light whitespace-wrap">
+                                        <td class="delete-button px-4 py-4 text-sm text-gray-900 font-light whitespace-wrap">
                                             <form action="{{ route('borrows.destroy', $borrow) }}" method="POST"
                                                 class="inline-block align-middle borrow-delete-form"
                                                 id="js-borrow-delete">
