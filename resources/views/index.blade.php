c15cb6d702f905c171848d1385ccbf43184d0ae5
shota <shota19830706@gmail.com>
2023-12-17 08:43:19 +0900

    minor changes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,8 +9,8 @@
                 </div>
             </div>
 
-            <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
-                <div class="py-4 inline-block min-w-full sm:px-6 lg:px-10">
+            <div class="overflow-x-auto -mx-6 lg:-mx-8">
+                <div class="py-4 block min-w-full px-6 lg:px-10">
                     <div class="overflow-hidden shadow-md rounded-lg">
                         <table class="min-w-full">
                             <thead class="bg-gray-200">
