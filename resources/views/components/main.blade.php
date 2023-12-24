eedc83d2dfdc14f42dd7aa892bdafaa34cac160a
shota <shota19830706@gmail.com>
2023-12-25 05:34:09 +0900

    Make the info-navigation a subview and load it in main.blade.php

--- resources/views/components/main.blade.php
+++ resources/views/components/main.blade.php
@@ -14,11 +14,8 @@
         @include('layouts.partials.auth-navigation')
         {{ $slot }}
     </div>
-    <nav class="md:mr-auto md:ml-4 md:py-1 md:pl-4 my-4 flex flex-wrap text-base justify-center">
-        <a class="mr-5 hover:text-gray-900">よくある質問</a>
-        <a class="mr-5 hover:text-gray-900">プライバシーポリシーと利用規約</a>
-        <a class="mr-5 hover:text-gray-900">お問い合わせ</a>
-    </nav>
+    {{-- info-navigation --}}
+    @include('layouts.partials.info-navigation')
 
     {{-- footer --}}
     @include('layouts.partials.footer')
---
+++ resources/views/layouts/partials/info-navigation.blade.php
@@ -0,0 +1,5 @@
+<nav class="md:mr-auto md:ml-4 md:py-1 md:pl-4 my-4 flex flex-wrap text-base justify-center">
+    <a class="mr-5 hover:text-gray-900">よくある質問</a>
+    <a class="mr-5 hover:text-gray-900">プライバシーポリシーと利用規約</a>
+    <a class="mr-5 hover:text-gray-900">お問い合わせ</a>
+</nav>
