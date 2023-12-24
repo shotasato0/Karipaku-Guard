f2d2223ad1982156e94053a6c27a87919a7c57fe
shota <shota19830706@gmail.com>
2023-12-25 08:56:58 +0900

    app.blade.phpのコンテンツ幅を、カスタムユーティリティクラスを使用して3/4に変更

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
 
 <body class="font-sans antialiased">
     @include('layouts.partials.header')
-    <div class="min-h-screen bg-gray-100">
+    <div class="min-h-three-quarters bg-gray-100">
         @include('layouts.navigation')
 
         <!-- Page Heading -->
