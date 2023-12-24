55ef7c8cda7140604803e15163a2fab928e9cd47
shota <shota19830706@gmail.com>
2023-12-25 08:25:31 +0900

    Create a custom utility class to set the height of 3/4 and define it in guest.blade.php

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,20 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
 
-/** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+            },
+            minHeight: {
+                "three-quarters": "75vh", // 画面の高さの75%に設定
             },
         },
     },
