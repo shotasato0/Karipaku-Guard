23214021933876f5edcf2986bbfe5a388b86a1eb
shota <shota19830706@gmail.com>
2024-01-02 05:34:53 +0900

    Adjust the position of the back link when the browser screen width is 768px or less

--- resources/views/components/back-link.blade.php
+++ resources/views/components/back-link.blade.php
@@ -1,4 +1,4 @@
 <a href="/" onclick="clearKeyword();"
-    class="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold mb-2 py-2 px-4 rounded transition duration-300 whitespace-nowrap">
+    class="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold mt-2 sm:mt-0 mb-2 mr-4 py-2 px-4 rounded transition duration-300 whitespace-nowrap">
     &laquo; 戻る
 </a>
