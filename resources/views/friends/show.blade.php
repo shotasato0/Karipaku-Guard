a64ad817ed88a737026b8aaffcae08dcbda163e2
shota <shota19830706@gmail.com>
2024-01-01 20:41:53 +0900

    Define overflow-x-auto in the show.blade.php table to make it a scrollable table

--- resources/views/friends/show.blade.php
+++ resources/views/friends/show.blade.php
@@ -8,26 +8,26 @@
                 <h1 class="text-3xl font-bold mb-6">
                     {{ $borrow->friend->name }}
                 </h1>
-                <div class="flex justify-between">
+                <div class="flex justify-between overflow-x-auto">
                     <table class="min-w-full">
                         <thead class="bg-gray-200">
                             <tr>
-                                <th class="px-4 py-2">年齢</th>
-                                <th class="px-4 py-2">性別</th>
-                                <th class="px-4 py-2">電話番号</th>
-                                <th class="px-4 py-2">Email</th>
-                                <th class="px-4 py-2">住所</th>
-                                <th class="px-4 py-2">あなたとの関係</th>
+                                <th class="px-4 py-2  whitespace-nowrap">年齢</th>
+                                <th class="px-4 py-2  whitespace-nowrap">性別</th>
+                                <th class="px-4 py-2  whitespace-nowrap">電話番号</th>
+                                <th class="px-4 py-2  whitespace-nowrap">Email</th>
+                                <th class="px-4 py-2  whitespace-nowrap">住所</th>
+                                <th class="px-4 py-2  whitespace-nowrap">あなたとの関係</th>
                             </tr>
                         </thead>
                         <tbody>
                             <tr>
-                                <td class="border px-4 py-2">{{ $borrow->friend->age ?? '未登録' }}</td>
-                                <td class="border px-4 py-2">{{ $borrow->friend->gender ?? '未登録' }}</td>
-                                <td class="border px-4 py-2">{{ $borrow->friend->phone ?? '未登録' }}</td>
-                                <td class="border px-4 py-2">{{ $borrow->friend->email ?? '未登録' }}</td>
-                                <td class="border px-4 py-2">{{ $borrow->friend->address ?? '未登録' }}</td>
-                                <td class="border px-4 py-2">{{ $borrow->friend->relationship_type ?? '未登録' }}</td>
+                                <td class="border whitespace-nowrap px-4 py-2">{{ $borrow->friend->age ?? '未登録' }}</td>
+                                <td class="border whitespace-nowrap px-4 py-2">{{ $borrow->friend->gender ?? '未登録' }}</td>
+                                <td class="border whitespace-nowrap px-4 py-2">{{ $borrow->friend->phone ?? '未登録' }}</td>
+                                <td class="border whitespace-nowrap px-4 py-2">{{ $borrow->friend->email ?? '未登録' }}</td>
+                                <td class="border whitespace-nowrap px-4 py-2">{{ $borrow->friend->address ?? '未登録' }}</td>
+                                <td class="border whitespace-nowrap px-4 py-2">{{ $borrow->friend->relationship_type ?? '未登録' }}</td>
                             </tr>
                         </tbody>
                     </table>
