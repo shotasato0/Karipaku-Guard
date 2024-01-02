78f00b2e609630ad5cc42d89962140b4b5ef50cf
shota <shota19830706@gmail.com>
2024-01-03 06:46:59 +0900

    Make the layout of result.blade.php the same as index.blade.php

--- resources/views/search/results.blade.php
+++ resources/views/search/results.blade.php
@@ -9,106 +9,106 @@
                     <x-create-button />
                 </div>
             </div>
-            <div class="overflow-x-auto sm:-mx-6 lg:-mx-10">
-                <div class="py-4 inline-block min-w-full sm:px-6 lg:px-10">
-                    <div class="overflow-hidden shadow-md rounded-lg">
-                        <table class="min-w-full">
-                            <thead class="bg-gray-200">
-                                <tr>
-                                    <th scope="col"
-                                        class="whitespace-nowrap text-lg  font-semibold text-gray-900 px-6 py-4 text-left">
-                                        借りた人の名前
-                                    </th>
-                                    <th scope="col"
-                                        class="whitespace-nowrap text-lg font-semibold text-gray-900 px-6 py-4 text-left">
-                                        借りた物の名前
-                                    </th>
-                                    <th scope="col"
-                                        class="whitespace-nowrap text-lg font-semibold text-gray-900 px-6 py-4 text-left">
-                                        借りた日
-                                    </th>
-                                    <th scope="col"
-                                        class="whitespace-nowrap text-lg font-semibold text-gray-900 px-6 py-4 text-left">
-                                        返却期限
-                                    </th>
-                                    <th scope="col"
-                                        class="whitespace-nowrap text-lg font-semibold text-gray-900 px-6 py-4 text-left">
-                                        経過日数
-                                    </th>
-                                    <th scope="col"
-                                        class="whitespace-nowrap text-lg font-semibold text-gray-900 px-6 py-4 text-left">
-                                        信頼度
-                                    </th>
-                                    <th scope="col"
-                                        class="whitespace-nowrap text-lg font-semibold text-gray-900 px-2 py-4 text-left">
+            <div class="overflow-x-auto rounded-lg shadow-md">
+                <table class="min-w-full divide-y divide-gray-300">
+                    <thead class="bg-gray-200 table-header-group">
+                        <tr>
+                            <th scope="col"
+                                class="px-6 py-4 text-left text-sm font-semibold whitespace-nowrap text-gray-700">
+                                借りた人の名前
+                            </th>
+                            <th scope="col"
+                                class="px-6 py-4 text-left text-sm font-semibold whitespace-nowrap text-gray-700">
+                                借りた物の名前
+                            </th>
+                            <th scope="col"
+                                class="px-6 py-4 text-left text-sm font-semibold whitespace-nowrap text-gray-700">
+                                借りた日
+                            </th>
+                            <th scope="col"
+                                class="px-6 py-4 text-left text-sm font-semibold whitespace-nowrap text-gray-700">
+                                返却期限
+                            </th>
+                            <th scope="col"
+                                class="px-12 py-4 text-left text-sm font-semibold whitespace-nowrap text-gray-700">
+                                信頼度
+                            </th>
+                            <th scope="col"
+                                class="px-4 py-4 text-left text-sm font-semibold whitespace-nowrap text-gray-700">
+                                編集
+                            </th>
+                            <th scope="col"
+                                class="px-4 py-4 text-left text-sm font-semibold whitespace-nowrap text-gray-700">
+                                削除
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody class="bg-white">
+                        @forelse ($posts as $borrow)
+                            <tr>
+                                <td data-label="借りた人"
+                                    class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                                    <a href="{{ route('borrows.friend', $borrow->id) }}" class="hover:text-blue-600">
+                                        {{ $borrow->friend->name }}
+                                    </a>
+                                </td>
+                                <td data-label="借りた物の名前"
+                                    class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                                    {{ $borrow->item_name }}
+                                </td>
+                                <td data-label="借りた日"
+                                    class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                                    {{ $borrow->borrowed_at->format('Y-m-d') }}
+                                </td>
+                                <td data-label="返却期限"
+                                    class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                                    @if ($borrow->deadline)
+                                        {{ $borrow->deadline->format('Y-m-d') }}
+                                    @else
+                                        {{ $borrow->deadline }}
+                                    @endif
+                                </td>
+
+                                <td data-label="信頼度"
+                                    class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900"
+                                    data-trust-score="{{ $borrow->days_until_deadline }}">
+                                    <img src="../../images/smile.png" alt="笑顔" class="w-16 sm:ml-4 js-smile">
+                                    <img src="../../images/bomb2.png" alt="爆弾"
+                                        class="w-16 sm:ml-4 hidden js-bomb">
+                                    <img src="../../images/ignition.png" alt="点火"
+                                        class="w-20 ml-1 hidden js-ignition">
+                                    <img src="../../images/explosion.png" alt="爆発"
+                                        class="w-24 hidden js-explosion">
+                                </td>
+
+                                <td class="edit-button pl-4 py-4 text-sm text-gray-900 font-light whitespace-wrap">
+                                    <a href="{{ route('borrows.edit', $borrow) }}"
+                                        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 rounded flex items-center justify-center h-10 w-20">
                                         編集
-                                    </th>
-                                    <th scope="col"
-                                        class="whitespace-nowrap text-lg font-semibold text-gray-900 py-4 text-left">
-                                        削除
-                                    </th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @forelse ($posts as $borrow)
-                                    <tr class="bg-white border-b">
-                                        <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                                            <a href="{{ route('borrows.friend', $borrow->id) }}"
-                                                class="hover:text-blue-600">
-                                                {{ $borrow->friend->name }}
-                                            </a>
-                                        </td>
-                                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                            {{ $borrow->item_name }}
-                                        </td>
-                                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                            {{ $borrow->borrowed_at->format('Y-m-d') }}
-                                        </td>
-                                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                            @if ($borrow->deadline)
-                                                {{ $borrow->deadline->format('Y-m-d') }}
-                                            @else
-                                                {{ $borrow->deadline }}
-                                            @endif
-                                        </td>
-                                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                            {{ $borrow->days_passed }}
-                                        </td>
-                                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                            {{ $borrow->trust_score }}
-                                        </td>
-                                        <td class="text-sm text-gray-900 font-light py-4 whitespace-wrap">
-                                            <a href="{{ route('borrows.edit', $borrow) }}"
-                                                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 rounded inline-block align-middle text-center"
-                                                style="line-height: normal; height: 38px; width: 80px;">
-                                                編集
-                                            </a>
-                                        </td>
-                                        <td class="text-sm text-gray-900 font-light py-4 whitespace-wrap">
-                                            <form action="{{ route('borrows.destroy', $borrow) }}" method="POST"
-                                                class="inline-block align-middle" id="js-borrow-delete">
+                                    </a>
+                                </td>
+                                <td class="delete-button px-4 py-4 text-sm text-gray-900 font-light whitespace-wrap">
+                                    <form action="{{ route('borrows.destroy', $borrow) }}" method="POST"
+                                        class="inline-block align-middle borrow-delete-form" id="js-borrow-delete">
 
-                                                @csrf
-                                                @method('DELETE')
+                                        @csrf
+                                        @method('DELETE')
 
-                                                <button type="submit"
-                                                    class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-                                                    style="line-height: normal; height: 38px; width: 80px;">
-                                                    削除
-                                                </button>
-                                            </form>
-                                        </td>
-                                    </tr>
-                                @empty
-                                    <tr class="bg-white border-b">
-                                        <td class=" text-center text-xl py-12" colspan="8">検索結果が見つかりませんでした
-                                        </td>
-                                    </tr>
-                                @endforelse
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
+                                        <button type="submit"
+                                            class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 rounded flex items-center justify-center h-10 w-20">
+                                            削除
+                                        </button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @empty
+                            <tr class="bg-white border-b">
+                                <td class=" text-center text-xl py-12" colspan="8">検索結果が見つかりませんでした
+                                </td>
+                            </tr>
+                        @endforelse
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
