24cc653a0028a2c56d5164d07d8e842e97555e82
shota <shota19830706@gmail.com>
2024-01-07 08:25:24 +0900

    Close the open pull-down menu when clicking on a screen other than the pull-down menu

--- resources/js/dropdown.js
+++ resources/js/dropdown.js
@@ -1,18 +1,36 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // すべてのメニューボタンの参照を取得
     const menuButtons = document.querySelectorAll("[id^='menu-button-']");
+    const dropdownMenus = document.querySelectorAll(".dropdown-menu");
 
+    // メニューボタンに対するクリックイベントリスナー
     menuButtons.forEach((button) => {
-        // 対応するドロップダウンメニューの参照を取得
-        const dropdownMenu = document.querySelector(
-            `.dropdown-menu[aria-labelledby='${button.id}']`
-        );
-
-        // ボタンクリック時のイベントリスナーを設定
-        button.addEventListener("click", function () {
-            // メニューの表示状態を切り替える
+        button.addEventListener("click", function (event) {
+            // プルダウンメニューの表示切り替え
+            const dropdownMenu = document.querySelector(
+                `.dropdown-menu[aria-labelledby='${button.id}']`
+            );
             dropdownMenu.style.display =
                 dropdownMenu.style.display === "none" ? "block" : "none";
+
+            // イベントの伝播を停止
+            event.stopPropagation();
         });
+    });
+
+    // ドキュメント全体のクリックイベントリスナー
+    document.addEventListener("click", function (event) {
+        const isClickInsideMenuButton = Array.from(menuButtons).some((button) =>
+            button.contains(event.target)
+        );
+        const isClickInsideDropdownMenu = Array.from(dropdownMenus).some(
+            (menu) => menu.contains(event.target)
+        );
+
+        if (!isClickInsideMenuButton && !isClickInsideDropdownMenu) {
+            // プルダウンメニュー以外をクリックした場合、全てのプルダウンメニューを閉じる
+            dropdownMenus.forEach((menu) => {
+                menu.style.display = "none";
+            });
+        }
     });
 });
