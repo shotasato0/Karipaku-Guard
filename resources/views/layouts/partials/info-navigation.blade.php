b0ac217320cae08c00efe49c2424797907d7914c
shota <shota19830706@gmail.com>
2024-01-12 08:56:54 +0900

    Improved navigation so that it is aligned vertically for SM size and smaller

--- resources/views/layouts/partials/info-navigation.blade.php
+++ resources/views/layouts/partials/info-navigation.blade.php
@@ -1,5 +1,6 @@
-<nav class="md:mr-auto md:ml-4 md:py-1 md:pl-4 my-4 flex flex-wrap text-base justify-center">
-    <a class="mr-5 hover:text-gray-900">よくある質問</a>
-    <a class="mr-5 hover:text-gray-900">プライバシーポリシーと利用規約</a>
-    <a class="mr-5 hover:text-gray-900">お問い合わせ</a>
+<nav
+    class="flex flex-col text-base justify-center items-center sm:items-start md:flex-row md:justify-center md:mr-auto md:ml-4 md:py-1 md:pl-4 my-4">
+    <a class="hover:text-gray-900 mb-2 md:mb-0 md:mr-5">よくある質問</a>
+    <a class="hover:text-gray-900 mb-2 md:mb-0 md:mr-5">プライバシーポリシーと利用規約</a>
+    <a class="hover:text-gray-900 mb-2 md:mb-0 md:mr-5">お問い合わせ</a>
 </nav>
