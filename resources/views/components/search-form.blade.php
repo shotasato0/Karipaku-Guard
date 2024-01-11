7d5d8cdfeb4cb82705763ad3ca9f7fb34244add9
shota <shota19830706@gmail.com>
2024-01-11 09:12:35 +0900

    Improved search box rounding in mobile view

--- resources/views/components/search-form.blade.php
+++ resources/views/components/search-form.blade.php
@@ -4,7 +4,7 @@
 
         <div class="relative mr-0 sm:mr-4">
             <input type="text" placeholder="検索" name="keyword" id="js-keyword"
-                class="px-4 py-2 border border-gray-300 rounded-l sm:rounded-md focus:outline-none focus:ring-2
+                class="px-4 py-2 border border-gray-300 rounded-l-md sm:rounded-md focus:outline-none focus:ring-2
                 focus:ring-blue-500 focus:border-transparent w-full">
             <input type="submit" value="検索"
                 class="absolute sm:right-0 sm:top-0 h-full bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold px-4 py-2 rounded-r-md cursor-pointer">
