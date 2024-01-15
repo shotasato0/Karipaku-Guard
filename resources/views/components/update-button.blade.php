328ea8c259f10b9c70baa328ea148a7e73bf3a59
shota <shota19830706@gmail.com>
2024-01-15 21:00:49 +0900

    Change the color of update button on mouse hover

--- resources/views/components/update-button.blade.php
+++ resources/views/components/update-button.blade.php
@@ -1,4 +1,4 @@
 <td class="px-6 py-4 whitespace-nowrap">
     <button type="submit"
-        class="bg-green-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded">更新</button>
+        class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-6 rounded">更新</button>
 </td>
