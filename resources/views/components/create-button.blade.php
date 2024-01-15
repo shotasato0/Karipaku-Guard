2dc5582678e5c9f096293c5559e6d26e9bf95767
shota <shota19830706@gmail.com>
2024-01-15 20:59:05 +0900

    Change the color of the new creation button when the mouse hovers

--- resources/views/components/create-button.blade.php
+++ resources/views/components/create-button.blade.php
@@ -1,4 +1,4 @@
-<a href="{{ route('borrows.create') }}" class="bg-green-500 hover:bg-blue-700 text-white text-center 
+<a href="{{ route('borrows.create') }}" class="bg-green-500 hover:bg-green-700 text-white text-center 
     font-bold py-2 px-4 ml-20 rounded-md  whitespace-nowrap h-10">
     新規作成
 </a>
