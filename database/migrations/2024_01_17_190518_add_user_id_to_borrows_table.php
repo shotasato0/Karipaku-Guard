d40ac3047d563f53418259cec63bc9a6fee37048
shota <shota19830706@gmail.com>
2024-01-17 19:08:44 +0900

    Add user_id column to generated migration file

--- database/migrations/2024_01_17_190518_add_user_id_to_borrows_table.php
+++ database/migrations/2024_01_17_190518_add_user_id_to_borrows_table.php
@@ -9,20 +9,24 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::table('borrows', function (Blueprint $table) {
-            //
-        });
-    }
+    public function up()
+{
+    Schema::table('borrows', function (Blueprint $table) {
+        $table->unsignedBigInteger('user_id')->after('id'); // idカラムの後に追加
+        $table->foreign('user_id')->references('id')->on('users'); // usersテーブルのidカラムを参照
+    });
+}
+
 
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
-        Schema::table('borrows', function (Blueprint $table) {
-            //
-        });
-    }
+    public function down()
+{
+    Schema::table('borrows', function (Blueprint $table) {
+        $table->dropForeign(['user_id']);
+        $table->dropColumn('user_id');
+    });
+}
+
 };
