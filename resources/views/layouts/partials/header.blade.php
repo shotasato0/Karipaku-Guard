4bd718567c69de9e3ab6fa51ef734b73c8370e9f
shota <shota19830706@gmail.com>
2024-01-19 07:53:52 +0900

    Clicking on the title logo now links to index.blade.php

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -1,6 +1,6 @@
 <header class="text-gray-600 body-font bg-gray-200">
     <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a href="/"
+        <a href="/index"
             class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
             <img src="../../images/bomb.png" alt="説明" class="w-14 sm:w-25 lg:w-40">
             <h1 class="ml-3 text-3xl font-bold">借りパクガード</h1>
