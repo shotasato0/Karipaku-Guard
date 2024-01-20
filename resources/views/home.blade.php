4866db9a9ca0c9555c4f1dcb63e49e9bd598b115
shota <shota19830706@gmail.com>
2024-01-20 21:49:42 +0900

    Adjust line breaks

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,9 +16,10 @@
                         <img src="../../images/bomb.png" alt="アプリのアイコン" class="w-14 sm:w-25 lg:w-40">
                         <h1 class="ml-3 text-3xl font-bold dark:text-gray-200">借りパクガード</h1>
                     </div>
-                    <h2 class="text-xl lg:text-2xl mt-6 mb-4 font-semibold">
-                        返却日を忘れることなし！借りパクガードで簡単管理
-                    </h2>
+                    <h1 class="text-xl lg:text-2xl mt-6 mb-4 font-semibold">
+                        返却日を忘れることなし！<br class="sm:hidden">
+                        借りパクガードで簡単管理
+                    </h1>
                     <p class="mb-8">
                         アプリでは、借りたものの情報と共に、相手の機嫌をアイコンで表現。普段は笑顔マークですが、期限が迫るにつれ、アイコンが変化。友人との関係を守るための強力なサポートツールです。
                     </p>
