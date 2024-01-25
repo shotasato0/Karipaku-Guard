3b616212e01a9552a747d0d81530e05f60869955
shota <shota19830706@gmail.com>
2024-01-25 21:13:55 +0900

    Delete unnecessary comments

--- resources/views/layouts/partials/info-navigation.blade.php
+++ resources/views/layouts/partials/info-navigation.blade.php
@@ -1,7 +1,6 @@
 <nav
     class="flex flex-col text-base justify-center items-center sm:items-start md:flex-row md:justify-center md:mr-auto md:ml-4 md:py-1 md:pl-4 my-4">
     <a href="{{ route('developer.message')}}" class="hover:text-gray-900 mb-2 md:mb-0 md:mr-5">開発者からのメッセージ</a>
-    {{-- <a href="{{ route('developer.message')}}" class="mr-5 hover:text-gray-900 font-bold">使い方</a> --}}
     <a class="hover:text-gray-900 mb-2 md:mb-0 md:mr-5">プライバシーポリシーと利用規約</a>
     <a class="hover:text-gray-900 mb-2 md:mb-0 md:mr-5">お問い合わせ</a>
 </nav>
