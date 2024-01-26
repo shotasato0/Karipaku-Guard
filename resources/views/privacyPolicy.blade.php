457d52d803c54c9023f283c57427e667c3a69270
shota <shota19830706@gmail.com>
2024-01-26 09:58:21 +0900

    Create privacy policy content

--- resources/views/privacyPolicy.blade.php
+++ resources/views/privacyPolicy.blade.php
@@ -3,6 +3,48 @@
     <div class="container mx-auto p-6 lg:p-20 text-black-800 dark:text-white">
         <h1 class="text-3xl font-bold mb-4">プライバシーポリシー</h1>
         <br>
-        
+
+        <h2 class="text-2xl font-bold mb-4 border-b">1. はじめに</h2>
+        <p>本プライバシーポリシーでは、[借りパクガード]（以下、「本アプリ」といいます）が、ユーザーの個人情報およびその他の情報をどのように
+            収集、使用、管理、保護するかについて説明します。</p><br>
+
+        <h2 class="text-2xl font-bold mb-4 border-b">2. 収集する情報</h2>
+        <p>本アプリが収集する情報には、以下のものが含まれますが、これに限定されません：</p>
+        <ul>
+            <li>個人情報（氏名、連絡先、住所など）</li>
+            <li>アプリ使用に関する情報（利用状況、設定など）</li>
+            <li>その他アプリの機能に必要な情報</li>
+        </ul><br>
+
+        <h2 class="text-2xl font-bold mb-4 border-b">3. 情報の利用目的</h2>
+        <p>収集した情報は、以下の目的で使用されます：</p>
+        <ul>
+            <li>アプリの機能提供</li>
+            <li>サービスの改善や新機能の開発</li>
+            <li>ユーザーサポート</li>
+            <li>法令遵守</li>
+        </ul><br>
+
+        <h2 class="text-2xl font-bold mb-4 border-b">4. 情報の共有と開示</h2>
+        <p>ユーザーの個人情報は、以下の場合を除き、第三者に共有または開示されません：</p>
+        <ul>
+            <li>ユーザーの同意がある場合</li>
+            <li>法的要求に応じる場合</li>
+            <li>アプリの運営に必要な業務委託先への提供</li>
+        </ul><br>
+
+        <h2 class="text-2xl font-bold mb-4 border-b">5. 情報の保護</h2>
+        <p>本アプリは、収集した情報の安全性を確保するために適切な物理的、技術的、組織的対策を講じます。</p><br>
+
+        <h2 class="text-2xl font-bold mb-4 border-b">6. ユーザーの権利</h2>
+        <p>ユーザーは、自己の個人情報に関して、アクセス、訂正、削除の要求を行うことができます。</p><br>
+
+        <h2 class="text-2xl font-bold mb-4 border-b">7. プライバシーポリシーの変更</h2>
+        <p>本プライバシーポリシーは、必要に応じて更新されることがあります。変更があった場合は、アプリ内またはウェブサイト上で通知します。</p><br>
+
+        <h2 class="text-2xl font-bold mb-4 border-b">8. お問い合わせ</h2>
+        <p>本プライバシーポリシーに関するご質問や懸念がある場合は、[お問合せのリンク]までご連絡ください。</p><br>
+
+
     </div>
 </x-main>
