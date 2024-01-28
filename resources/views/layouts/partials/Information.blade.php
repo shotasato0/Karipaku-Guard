2d77d392ce81efd94e2918be969b1734e344cd5d
shota <shota19830706@gmail.com>
2024-01-28 21:37:56 +0900

    Adjusted the behavior of information links when hovering

--- resources/views/layouts/partials/Information.blade.php
+++ resources/views/layouts/partials/Information.blade.php
@@ -1,6 +1,6 @@
 <nav
     class="flex flex-col text-base justify-center items-center sm:items-start md:flex-row md:justify-center md:mr-auto md:ml-4 md:py-1 md:pl-4 my-4">
-    <a href="{{ route('information.developerMessage') }}" class="hover:text-gray-900 mb-2 md:mb-0 md:mr-5">開発者からのメッセージ</a>
-    <a href="{{ route('information.privacy-policy') }}" class="hover:text-gray-900 mb-2 md:mb-0 md:mr-5">プライバシーポリシー</a>
-    <a href="{{ route('information.terms') }}" class="hover:text-gray-900 mb-2 md:mb-0 md:mr-5">利用規約</a>
+    <a href="{{ route('information.developerMessage') }}" class="hover:text-gray-500 duration-75 mb-2 md:mb-0 md:mr-5">開発者からのメッセージ</a>
+    <a href="{{ route('information.privacy-policy') }}" class="hover:text-gray-500 duration-75 mb-2 md:mb-0 md:mr-5">プライバシーポリシー</a>
+    <a href="{{ route('information.terms') }}" class="hover:text-gray-500 duration-75 mb-2 md:mb-0 md:mr-5">利用規約</a>
 </nav>
