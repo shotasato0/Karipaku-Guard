d42f06dd5b32204342affe422fe2f6a2a1dc10e4
shota <shota19830706@gmail.com>
2024-01-30 17:59:56 +0900

    Write settings for cooperation with Google in service/config.app

--- config/services.php
+++ config/services.php
@@ -31,4 +31,11 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'google' => [
+        'client_id' => env('GOOGLE_KEY'), 
+        'client_secret' => env('GOOGLE_SECRET'), 
+        'redirect' => env('GOOGLE_REDIRECT_URI'), 
+      ],
+
+
 ];
