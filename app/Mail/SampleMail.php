cb9b7079ecf0d90239bbff55badedc3ab631cd29
shota <shota19830706@gmail.com>
2024-01-31 09:17:01 +0900

    Rewrite the contents of SampleMail.php

--- app/Mail/SampleMail.php
+++ app/Mail/SampleMail.php
@@ -5,8 +5,6 @@ namespace App\Mail;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
-use Illuminate\Mail\Mailables\Content;
-use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
 class SampleMail extends Mailable
@@ -15,39 +13,27 @@ class SampleMail extends Mailable
 
     /**
      * Create a new message instance.
-     */
-    public function __construct()
-    {
-        //
-    }
-
-    /**
-     * Get the message envelope.
-     */
-    public function envelope(): Envelope
-    {
-        return new Envelope(
-            subject: 'Sample Mail',
-        );
-    }
-
-    /**
-     * Get the message content definition.
-     */
-    public function content(): Content
-    {
-        return new Content(
-            view: 'view.name',
-        );
-    }
-
-    /**
-     * Get the attachments for the message.
      *
-     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     * @return void
      */
-    public function attachments(): array
+    public function __construct($name, $email)
     {
-        return [];
+        $this->name = $name;
+        $this->email = $email;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->to($this->email)
+            ->subject('サンプルタイトル')
+            ->view('mail.sample')
+            ->with([
+                'name' => $this->name,
+            ]);
     }
 }
