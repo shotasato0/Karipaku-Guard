8548d4a7aba6848b847696fd070cb522b844460e
shota <shota19830706@gmail.com>
2024-02-01 06:24:30 +0900

    Set favicon

--- resources/views/layouts/partials/head.blade.php
+++ resources/views/layouts/partials/head.blade.php
@@ -6,6 +6,8 @@
 <!-- Fonts -->
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+<link rel="icon" href="{{ asset('favicon.ico') }}" type="image/ico">
+
 
 <!-- Styles -->
 <style>
