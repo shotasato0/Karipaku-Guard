822e1b6e2aa0d22b3e1912541d80fb8357c0e4f6
shota <shota19830706@gmail.com>
2024-02-03 17:08:46 +0900

    Add comment

--- database/migrations/2024_02_03_164229_add_user_id_to_borrows_table.php
+++ database/migrations/2024_02_03_164229_add_user_id_to_borrows_table.php
@@ -8,9 +8,13 @@ return new class extends Migration
 {
     public function up()
     {
+        // 'borrows'テーブルに新しいカラムと外部キーを追加
         Schema::table('borrows', function (Blueprint $table) {
+            // 'user_id'カラムがまだ存在しないかチェック
             if (!Schema::hasColumn('borrows', 'user_id')) {
+                // 'id'カラムの後に'user_id'カラムを追加
                 $table->unsignedBigInteger('user_id')->after('id');
+                // 'user_id'を外部キーとして定義し、'users'テーブルの'id'カラムを参照。削除時はカスケード
                 $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             }
         });
@@ -18,8 +22,11 @@ return new class extends Migration
 
     public function down()
     {
+        // up()で行った操作を逆に行う
         Schema::table('borrows', function (Blueprint $table) {
+            // 'user_id'に関する外部キー制約を削除
             $table->dropForeign(['user_id']);
+            // 'user_id'カラムを削除
             $table->dropColumn('user_id');
         });
     }
