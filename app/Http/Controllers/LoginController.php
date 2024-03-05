5b43e1db97ca9343414ba2d76b34de68163fa329
shota <shota19830706@gmail.com>
2024-03-06 06:39:45 +0900

    Temporarily comment out guest user creation logic (may be removed later)

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -1,24 +1,24 @@
 <?php
 
-namespace App\Http\Controllers;
+// namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Models\User;
-use Illuminate\Support\Facades\Auth;
-use App\Providers\RouteServiceProvider;
+// use Illuminate\Http\Request;
+// use App\Models\User;
+// use Illuminate\Support\Facades\Auth;
+// use App\Providers\RouteServiceProvider;
 
 
-class LoginController extends Controller
-{
-    public function guest() {
-        // Userテーブルからゲストユーザーのデータを取得。条件にはメアド。
-        $user = User::firstOrCreate(
-            ['email' => 'guest@example.com'], // 検索条件
-            ['is_guest' => true], // 作成する場合のデフォルト値
-        );
+// class LoginController extends Controller
+// {
+//     public function guest() {
+//         // Userテーブルからゲストユーザーのデータを取得。条件にはメアド。
+//         $user = User::firstOrCreate(
+//             ['email' => 'guest@example.com'], // 検索条件
+//             ['is_guest' => true], // 作成する場合のデフォルト値
+//         );
 
-        Auth::login($user);
+//         Auth::login($user);
 
-        return redirect()->intended(RouteServiceProvider::HOME);
-    }
-}
+//         return redirect()->intended(RouteServiceProvider::HOME);
+//     }
+// }
--- database/seeders/GuestUserSeeder.php
+++ database/seeders/GuestUserSeeder.php
@@ -1,24 +1,22 @@
 <?php
 
-namespace Database\Seeders;
+// namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\DB;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+// use Illuminate\Database\Seeder;
+// use Illuminate\Support\Facades\Hash;
+// use Illuminate\Support\Facades\DB;
 
-class GuestUserSeeder extends Seeder
-{
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
-    {
-        DB::table('users')->insert([
-            'name' => 'ゲストユーザー',
-            'email' => 'guest@example.com',
-            'password' => Hash::make('XDrzn4BwRHwu'), //適切なパスワードを設定
-            'is_guest' => true, // ゲストユーザーとして設定
-        ]);
-    }
-}
+// class GuestUserSeeder extends Seeder
+// {
+   
+//     public function run(): void
+//     {
+//         DB::table('users')->insert([
+//             'name' => 'ゲストユーザー',
+//             'email' => 'guest@example.com',
+//             'password' => Hash::make('XDrzn4BwRHwu'),
+//             'is_guest' => true,
+//         ]);
+//     }
+// }
