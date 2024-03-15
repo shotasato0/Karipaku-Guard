9c29a0cfb960eae8d3334f2cacf04d34e71f4e88
shota <shota19830706@gmail.com>
2024-03-15 22:00:27 +0900

    Reload page on back or forward navigation

--- resources/js/historyHandler.js
+++ resources/js/historyHandler.js
@@ -1,4 +1,8 @@
+// DOMContentLoadedイベントが発生した時（HTMLドキュメントが読み込まれ、解析が完了した時）に、指定された関数を実行
 document.addEventListener("DOMContentLoaded", function () {
+    // window.performanceとwindow.performance.navigationが存在し、
+    // ユーザーが「戻る」または「進む」ボタンでページにアクセスした場合、
+    // ページをリロードする
     if (window.performance && window.performance.navigation.type === window.performance.navigation.TYPE_BACK_FORWARD) {
         window.location.reload();
     }
